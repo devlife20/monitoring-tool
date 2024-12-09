646fb8d8a112b5c798e776fb1136865cf736290b
adufoemma <adufoemmanuel@gmail.com>
2024-12-09 18:45:57 +0000

    feat: implement a basic add command

---
+++ cmd/sources/add.go
@@ -0,0 +1,32 @@
+package sources
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	url string
+)
+
+// AddCmd represents the add command
+var AddCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add a log source",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+}
+
+func init() {
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	AddCmd.PersistentFlags().StringVarP(&url, "url", "u", "", "The url to ping")
+	SourceCmd.AddCommand(AddCmd)
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
