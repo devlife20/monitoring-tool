4aa6b816e8b502bf8e901ed30396805966da0e13
adufoemma <adufoemmanuel@gmail.com>
2025-01-20 17:49:27 +0000

    add : add watchCmd.go

---
+++ cmd/watchLogs/watchCmd.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package watchLogs
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// WatchCmd represents the watch command
+var WatchCmd = &cobra.Command{
+	Use:   "watch",
+	Short: "aggregate logs from different sources in real time or on a scheduled basis",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			_ = cmd.Help()
+		}
+	},
+}
+
+func init() {
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// watchCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// watchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
