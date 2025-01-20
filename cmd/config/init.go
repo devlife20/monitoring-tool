44933a4f84488e95985ac7da37d1ef8fe72cf911
adufoemma <adufoemmanuel@gmail.com>
2025-01-20 18:08:45 +0000

    feature : add log init command to initialise configuration

---
+++ cmd/config/init.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package config
+
+import (
+	"github.com/devlife20/monitoring-tool/config"
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize configuration",
+	Long: `Initialize the configuration file with Elasticsearch credentials.
+Creates a config file in ~/.config/monit/config.yaml`,
+	Run: func(cmd *cobra.Command, args []string) {
+		config.SetConfig()
+	},
+}
+
+func init() {
+	MonitConfig.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
