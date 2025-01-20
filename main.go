4208e1030c3e25999cdd5b41422314bbd87fb2b0
adufoemma <adufoemmanuel@gmail.com>
2025-01-20 17:40:01 +0000

    chore : create config path on app startup

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/devlife20/monitoring-tool/cmd"
+import (
+	"github.com/devlife20/monitoring-tool/cmd"
+	utilities "github.com/devlife20/monitoring-tool/utilies"
+	"log"
+)
 
 func main() {
+	err := utilities.CreateConfigPath()
+	if err != nil {
+		log.Fatalf("Error creating config path: %v", err)
+	}
 	cmd.Execute()
 }
