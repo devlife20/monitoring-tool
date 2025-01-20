b12db1ecf7d8139d60555a04d7a3abe5fe24ff60
adufoemma <adufoemmanuel@gmail.com>
2025-01-20 17:35:32 +0000

    feature(command): implement configure command

---
+++ cmd/configure.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/devlife20/monitoring-tool/config"
+	"github.com/spf13/cobra"
+)
+
+// configureCmd represents the configure command
+var configureCmd = &cobra.Command{
+	Use:   "configure",
+	Short: "A brief description of your command",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		config.AwsConfigure()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(configureCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// configureCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// configureCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
